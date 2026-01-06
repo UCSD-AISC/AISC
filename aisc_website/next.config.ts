7fd0f9e87c0ce882a9468ae0d5c34ac8015e2a41
r800360 <rosachdeva@ucsd.edu>
2026-01-06 15:51:41 -0800

    chore: further build fixes

--- aisc_website/next.config.ts
+++ aisc_website/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+
+  images: {
+    unoptimized: true, // required for static export with next/image
+  },
 };
 
 export default nextConfig;
