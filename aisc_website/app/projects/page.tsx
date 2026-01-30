794a34dd79dc80b81ea7b3aa5d8e4d0317aadede
sydverma <emailsydverma@gmail.com>
2026-01-30 15:25:43 -0800

    Made the buttons click and work for projects

--- aisc_website/app/projects/page.tsx
+++ aisc_website/app/projects/page.tsx
@@ -1,18 +1,23 @@
-import Image from "next/image";
+"use client"
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import projects from "@/lib/projects.json";
 import Card from "@/components/Card/Card";
+import { useState } from "react";
 
 export default function ProjectsPage() {
+  const [status, setStatus] = useState<"Active" | "Recruiting" | "Completed" | null>(null);
+
   return (
     <>
       <Navbar />
-      <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Active</button>
-      <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Recruiting</button>
-      <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Archived</button>
+      <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" onClick={() => setStatus("Active")}>Active</button>
+      <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" onClick={() => setStatus("Recruiting")}>Recruiting</button>
+      <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none" onClick={() => setStatus("Completed")}>Archived</button>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {projects.map((card) => (
+
+        {projects.filter(card => card.status === status).
+        map((card) => (
         <Card
           key={card.index} // Essential for list rendering
           title={card.title}
