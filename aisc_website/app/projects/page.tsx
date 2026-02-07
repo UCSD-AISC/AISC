c764b7c8b9583bbc7502cd89a6a6e603a03bac0b
Jaeheek0319 <stampy6000@gmail.com>
2026-02-07 02:52:42 -0800

    implemented modal, added hero, added hover animation, updated button ui

--- aisc_website/app/projects/page.tsx
+++ aisc_website/app/projects/page.tsx
@@ -3,8 +3,8 @@ import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 import projects from "@/lib/projects.json";
 import Card from "@/components/Card/Card";
+import education from "@/public/education.png";
 import { useState } from "react";
-import Image from "next/image";
 
 export default function ProjectsPage() {
   const [status, setStatus] = useState<"Active" | "Recruiting" | "Completed" | null>(null);
@@ -12,27 +12,40 @@ export default function ProjectsPage() {
   return (
     <>
       <Navbar />
-      <section
-        className="relative text-white px-6 md:px-50 py-50"
+      
+      <section 
+        className="relative w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] -mt-[200px]  h-[550px] bg-gradient-to-r from-[#0f172a] via-[#3b0f2d] to-[#0f2a2f]"
         style={{
-          backgroundImage: "url('/education.png')",
+          backgroundImage: `url(${education.src})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-      </section>
+         <div className="absolute inset-0 bg-gradient-to-b from-black/10 via-black/5 to-transparent" />
 
-      <div className = "flex justify-center gap-4 scale-105">
-        <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-2xl text-sm px-4 py-2.5 focus:outline-none leading-5" onClick={() => setStatus("Active")}>Active</button>
-        <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-2xl text-sm px-4 py-2.5 focus:outline-none leading-5" onClick={() => setStatus("Recruiting")}>Recruiting</button>
-        <button type="button" className="text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-neutral-tertiary shadow-xs font-medium leading-5 rounded-2xl text-sm px-4 py-2.5 focus:outline-none leading-5" onClick={() => setStatus("Completed")}>Archived</button>
+        <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-b from-transparent to-[#0f172a]" />
+
+
+        <h1 className="absolute top-[180px] left-1/2 -translate-x-1/2 text-center px-6 text-4xl sm:text-5xl md:text-7xl font-semibold text-purple-400 animate-fade-in-hard">
+          Projects
+        </h1>
+       <h1 className="absolute top-[350px] left-1/2 -translate-x-1/2 text-center px-6 text-4xl sm:text-5xl md:text-4xl  text-gray-100 drop-shadow animate-fade-in-hard">
+          Take A Look At What AISC Is Doing
+        </h1>
+      </section>
+      
+      
+      <div className = "flex justify-center gap-8 scale-105 ">
+        <button type="button" className={`text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-purple-600/60 shadow-xs font-medium leading-5 rounded-2xl text-sm px-4 py-2.5 focus:outline-none leading-5 hover:scale-[1.1] transition ${status === "Active" ? "bg-purple-600/90 shadow-[0_0_20px_rgba(168,85,247,0.25)]" : ""}`} onClick={() => setStatus("Active")}>Active</button>
+        <button type="button" className={`text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-purple-600/60 shadow-xs font-medium leading-5 rounded-2xl text-sm px-4 py-2.5 focus:outline-none leading-5 hover:scale-[1.1] transition ${status === "Recruiting" ? "bg-purple-600/90 shadow-[0_0_20px_rgba(168,85,247,0.25)]" : ""}`} onClick={() => setStatus("Recruiting")}>Recruiting</button>
+        <button type="button" className={`text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading focus:ring-4 focus:ring-purple-600/60 shadow-xs font-medium leading-5 rounded-2xl text-sm px-4 py-2.5 focus:outline-none leading-5 hover:scale-[1.1] transition ${status === "Completed" ? "bg-purple-600/90 shadow-[0_0_20px_rgba(168,85,247,0.25)]" : ""}`} onClick={() => setStatus("Completed")}>Archived</button>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
 
         {projects.filter(card => card.status === status || status === null
-        ).map((card) => (
+        ).map((card) => ( 
         <Card
-          key={card.index} // Essential for list rendering
+          key={card.index}
           title={card.title}
           cardDescription={card.cardDescription}
           contributors={card.contributors}
@@ -41,6 +54,7 @@ export default function ProjectsPage() {
           status={card.status}
           difficulty={card.difficulty}
           img={card.img}
+          modalDescription={card.modalDescription}
         />
       ))}
         </div>
--- aisc_website/components/Card/Card.tsx
+++ aisc_website/components/Card/Card.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import { useState } from "react";
+import Modal from "../Modal/Modal";
 
 type project = {
   title: string;
   cardDescription: string;
+  modalDescription: string;
   contributors: string[];
   link: string;
   stack: string;
@@ -14,32 +17,56 @@ type project = {
 
 
 const Card = (project: project) => {
-  const renderDifficulty = () => {
-      if (project.difficulty === "Easy") {
-        return <p className="text-body mb-6 flex justify-center items-center rounded-full bg-green-600 px-2 w-fit text-white-900">{project.difficulty}</p>;
-      } else if (project.difficulty === "Medium") {
-        return <p className="text-body mb-6 flex justify-center items-center rounded-full bg-yellow-600 px-2 w-fit text-white-900">{project.difficulty}</p>;
+  const renderDifficulty = (difficulty: string ) => {
+      if (difficulty === "Easy") {
+        return <p className="text-body mb-6 flex justify-center items-center rounded-full bg-green-600 px-2 w-fit text-white-900">{difficulty}</p>;
+      } else if (difficulty === "Medium") {
+        return <p className="text-body mb-6 flex justify-center items-center rounded-full bg-yellow-600 px-2 w-fit text-white-900">{difficulty}</p>;
       } else {
-        return <p className="text-body mb-6 flex justify-center items-center rounded-full bg-red-600 px-2 w-fit text-white-900">{project.difficulty}</p>;
+        return <p className="text-body mb-6 flex justify-center items-center rounded-full bg-red-600 px-2 w-fit text-white-900">{difficulty}</p>;
       }
-    };  
+    }; 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    console.log("Modal Opened");
+
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  } 
   return (
     <div className="flex justify-center">
-      <div className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-2xl shadow-xs animate-fade-in">
-        <div className="relative w-full h-44 mb-6 rounded-base overflow-hidden">
+      <div className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-2xl shadow-xs animate-fade-in-soft hover:-translate-y-3 transition-all duration-200" onClick={() => openModal()}> 
+        <div className="relative w-full h-44 mb-6  bg-white shadow-sm rounded-2xl">
           <Image
             src={project.img}
             alt={project.title}
             fill
-            className="object-cover"
+            className="object-contain"
           />
         </div>
-        <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8 flex justify-center items-center">{project.title}</h5>
+        <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8 flex justify-center items-center text-indigo-300
+">{project.title}</h5>
         <div className="flex flex-row justify-center items-center">
-          {renderDifficulty()}
+          {renderDifficulty(project.difficulty)}
         </div>
         <p className="text-body mb-6 flex justify-center items-center">{project.cardDescription}</p>
     </div>
+    {isModalOpen && <Modal 
+    closeModal={closeModal} 
+    renderDifficulty={renderDifficulty}
+    modalDescription={project.modalDescription} 
+    contributors={project.contributors} 
+    link={project.link} 
+    stack={project.stack} 
+    status={project.status} 
+    difficulty={project.difficulty} 
+    img={project.img} 
+    title={project.title} 
+    />}
     </div>
   );
 }
