06cfcd932797cf4c18f60566b55218b077e0be11
Jaeheek0319 <stampy6000@gmail.com>
2026-02-14 02:34:36 -0800

    removed changes to project cards

--- aisc_website/components/Card/Card.tsx
+++ aisc_website/components/Card/Card.tsx
@@ -38,16 +38,9 @@ const Card = (project: project) => {
     setIsModalOpen(false);
   } 
   return (
-  <div className="flex justify-center">
-    <div className="relative max-w-sm w-full h-full">
-
-      <div className="absolute inset-0 translate-x-3 translate-y-3 rounded-2xl bg-blue-400 border border-default -z-10" />
-
-      <div
-        className="relative w-full bg-red-400 p-6 border border-default rounded-2xl shadow-xs animate-fade-in-soft transition-all duration-200 hover:-translate-y-3 hover:-translate-x-4 h-full flex flex-col cursor-pointer"
-        onClick={openModal}
-      >
-        <div className="relative w-full h-44 mb-6 bg-white shadow-sm rounded-2xl">
+    <div className="flex justify-center">
+      <div className="bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-2xl shadow-xs animate-fade-in-soft hover:-translate-y-3 transition-all duration-200" onClick={() => openModal()}> 
+        <div className="relative w-full h-44 mb-6  bg-white shadow-sm rounded-2xl">
           <Image
             src={project.img}
             alt={project.title}
@@ -55,37 +48,27 @@ const Card = (project: project) => {
             className="object-contain"
           />
         </div>
-
-        <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8 flex justify-center items-center text-indigo-300">
-          {project.title}
-        </h5>
-
+        <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8 flex justify-center items-center text-indigo-300
+">{project.title}</h5>
         <div className="flex flex-row justify-center items-center">
           {renderDifficulty(project.difficulty)}
         </div>
-
-        <p className="text-body mb-6 flex justify-center items-center flex-grow">
-          {project.cardDescription}
-        </p>
-      </div>
-
-      {isModalOpen && (
-        <Modal
-          closeModal={closeModal}
-          renderDifficulty={renderDifficulty}
-          modalDescription={project.modalDescription}
-          contributors={project.contributors}
-          link={project.link}
-          stack={project.stack}
-          status={project.status}
-          difficulty={project.difficulty}
-          img={project.img}
-          title={project.title}
-        />
-      )}
+        <p className="text-body mb-6 flex justify-center items-center">{project.cardDescription}</p>
     </div>
-  </div>
-);
+    {isModalOpen && <Modal 
+    closeModal={closeModal} 
+    renderDifficulty={renderDifficulty}
+    modalDescription={project.modalDescription} 
+    contributors={project.contributors} 
+    link={project.link} 
+    stack={project.stack} 
+    status={project.status} 
+    difficulty={project.difficulty} 
+    img={project.img} 
+    title={project.title} 
+    />}
+    </div>
+  );
 }
 
 export default Card;
